677226b688401fc462830b8090ea0a3d335d7996
Firoz Tyagi <firoztyagi1783@gmail.com>
2021-10-15 13:52:19 +0530

    Tours added

--- src/script/view/tourView.js
+++ src/script/view/tourView.js
@@ -3,55 +3,47 @@ export class TourView {
     this.tourContainer = document.querySelector(".tour-container");
   }
   generateMarkup(tour) {
-    const ratings = Math.round(tour.averageRatings);
     const markup = `<div class="tour">
-      <div class="tour-left">
-        <img class="tour-img" src="./src/img/img.jpg" alt="" />
-        <span class="tour-img-gradient"></span>
+    <div class="tour-stars">
+      <i class="far fa-star"></i>
+      <i class="far fa-star"></i>
+      <i class="far fa-star"></i>
+      <i class="far fa-star"></i>
+      <i class="far fa-star"></i>
+    </div>
+    <div class="tour-img">
+      <img src="./src/img/img.jpg" alt="">
+      <span class="img-linear-bg"></span>
+      <p class="tour-title">${tour.name}</p>
+    </div>
+    <div class="tour-details">
+      <ul class="detail-list">
+        <li class="tour-detail-list-item">
+          <i class="far fa-flag"></i>
+        <p>${tour.location}</p>
+        </li>
+        <li class="tour-detail-list-item">
+          <i class="far fa-user"></i>
+          <p>${tour.groupSize}</p>
+        </li>
+        <li class="tour-detail-list-item">
+          <i class="far fa-clock"></i>
+          <p>${tour.tourLength}</p>
+        </li>
+        <li class="tour-detail-list-item">
+          <i class="far fa-calendar-alt"></i>
+          <p>${new Date(tour.dates[0]).getFullYear()}</p>
+        </li>
+      </ul>
+    </div>
+      <div class="tour-bottom-details">
+        <div class="tour-price">
+          <span>Only</span>
+          <p>$${tour.price}</p>
+        </div>
+        <a href="#" class="tour-detail-btn">Details</a>
       </div>
-      <div class="tour-right">
-        <div class="tour-ratings">
-          <div class="tour-stars">
-            (${tour.totalRatings})
-          </div>
-          <p class="tour-price"><span class="dollar">$</span>${tour.price}</p>
-        </div>
-        <p class="tour-title">${tour.name}</p>
-        <div class="tour-info">
-          <div class="tour-info-box tour-info-location">
-            <i class="far fa-flag"></i>
-            <p>${tour.location}</p>
-          </div>
-          <div class="tour-info-box tour-info-groupSize">
-            <i class="far fa-user"></i>
-            <p>${tour.groupSize}</p>
-          </div>
-          <div class="tour-info-box tour-info-date">
-            <i class="far fa-calendar-alt"></i>
-            <p>${new Date(tour.dates[0]).getFullYear()}, ${new Date(
-      tour.dates[0]
-    ).getUTCMonth()}</p>
-          </div>
-          <div class="tour-info-box tour-info-duration">
-            <i class="far fa-hourglass"></i>
-            <p>${tour.difficulty}</p>
-          </div>
-        </div>
-        <p class="tour-description">
-          ${tour.info}
-        </p>
-        <div class="tour-btn">
-          <a href="#" class="tour-link">Details</a>
-        </div>
-      </div>
-    </div>`;
-
+  </div>`;
     this.tourContainer.insertAdjacentHTML("afterbegin", markup);
-    const tourRating = document.querySelector(".tour-stars");
-    const star = `<i class="far fa-star"></i>`;
-    for (let i = 0; i < ratings; i++) {
-      tourRating.insertAdjacentHTML("afterbegin", star);
-    }
-    return markup;
   }
 }
