d7e5f9f5a7a86d75690a787fe82aca689f1543fe
Firoz Tyagi <firoztyagi1783@gmail.com>
2021-11-08 20:33:53 +0530

    Added S3 urls

--- src/script/view/myBookingsView.js
+++ src/script/view/myBookingsView.js
@@ -1,4 +1,5 @@
 import { _DOMAIN } from "../model/model";
+import { HOSTING } from "../model/model";
 
 export class MyBookingsView {
   constructor() {
@@ -8,7 +9,7 @@ export class MyBookingsView {
     const markup = ` <div class="tour">
     <div class="tour-header">
       <span class="image-overlay"></span>
-      <img src="${_DOMAIN}/${tour.headImg}" alt="" class="tour-img">
+      <img src="${HOSTING}${tour.headImg}" alt="" class="tour-img">
     </div>
     <div class="tour-content">
       <p class="tour-title">${tour.name}</p>
