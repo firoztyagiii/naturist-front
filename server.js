2d114ec035d2703e12dcdb1bf1feea52e657a0ae
Firoz Tyagi <firoztyagi1783@gmail.com>
2021-11-13 22:59:07 +0530

    PORT fixed

--- server.js
+++ server.js
@@ -2,10 +2,7 @@ const express = require("express");
 const app = express();
 
 app.use(express.static("public"));
-app.use("*", (req, res) => {
-  res.send("./index.html");
-});
 
-app.listen(3000, () => {
-  console.log("Listening to PORT 3000");
+app.listen(2000, () => {
+  console.log("Listening to PORT 2000");
 });
